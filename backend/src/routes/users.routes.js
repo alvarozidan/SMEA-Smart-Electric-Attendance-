4b44a6925abce0fac1cc0538c5aa7c73314c2aeb
Alvaro Zidane <alvarozidane67@gmail.com>
2026-07-26 18:43:00 +0700

    change: ikon aplikasi sama nama aplikasi

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authenticate = require('../middlewares/auth.middleware');
 const { requireRole } = require('../middlewares/rbac.middleware');
 const usersController = require('../controllers/users.controller');
-const { useReducer } = require('react');
 
 router.use(authenticate);
 
--- mobile/lib/core/constant/api_constant.dart
+++ mobile/lib/core/constant/api_constant.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'http://10.10.11.2:3000/api/v1';
+  static const String baseUrl = 'http://192.168.43.38:3000/api/v1';
 
   //Auth
   static const String login = '/auth/login';
